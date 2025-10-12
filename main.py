d69406339a919c28145c6f0146232281d65a77fd
Arjav Patel <arjav1528@gmail.com>
2025-10-13 02:53:17 +0530

    Hello World

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn as skl
+
+# print("Libraries imported.")
+
+
+
+cost_of_living = pd.read_csv('cost_of_living.csv')
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+
+
+
